b38780c70bf86286d485a915d931a9ffff715dc8
Luciano <lucianoalejandromas@gmail.com>
2023-03-24 00:48:57 -0300

    "agregar css de favoritos a la card"

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,13 +2,22 @@ import styles from "../Card/card.module.css"
 import { Link } from "react-router-dom"
 import { useDispatch } from "react-redux"
 import { removeCharacter } from "../../redux/actions/actions"
+import DeleteIcon from "@mui/icons-material/Delete"
+import FavoriteIcon from "@mui/icons-material/Favorite"
 export default function Card(props) {
   const dispatch = useDispatch()
   return (
     <div className={styles.card}>
       <img src={props.image} alt="" />
       <div className={`${styles.btn} btn`}>
-        <button onClick={() => dispatch(removeCharacter(props.name))}>x</button>
+        <div className={styles.buttonFlex}>
+          <button>
+            <FavoriteIcon className={styles.heartIcon} />
+          </button>
+          <button onClick={() => dispatch(removeCharacter(props.name))}>
+            <DeleteIcon className={styles.trashIcon} />
+          </button>
+        </div>
       </div>
       <div className={styles.info}>
         <Link to={`/detail/${props.id}`} key={props.id}>
--- src/components/Characters/CharactersSection.jsx
+++ src/components/Characters/CharactersSection.jsx
@@ -3,24 +3,25 @@ import Cards from "../Cards/Cards"
 import styles from "../Characters/Characters.module.css"
 import Modal from "../Modal/Modal"
 import { useState } from "react"
-import { connect } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { addCharacter } from "../../redux/actions/actions"
-import { removeCharacter } from "../../redux/actions/actions"
 
-export function Characters(props) {
+export default function Characters(props) {
   const [errorMessage, setErrorMessage] = useState("")
   const [showModal, setShowModal] = useState(false)
+  const dispatch = useDispatch()
+  const state = useSelector((state) => state.characters)
 
   function onSearch(id) {
     fetch(`https://rickandmortyapi.com/api/character/${id}`)
       .then((res) => res.json())
       .then((data) => {
-        const isCharacterExist = props.characters.some(
+        const isCharacterExist = state.some(
           (character) => character.id === data.id
         )
         if (!isCharacterExist) {
           data.name
-            ? props.addCharacter(data)
+            ? dispatch(addCharacter(data))
             : showErrorModal("No se encontró el personaje")
         } else {
           showErrorModal(`El personaje "${data.name}" ya existe`)
@@ -52,24 +53,24 @@ export function Characters(props) {
         <SearchBar onSearch={onSearch} />
       </div>
       <div className={styles.grid}>
-        <Cards characters={props.characters} />
+        <Cards characters={state} />
       </div>
       {showModal && <Modal message={errorMessage} onClose={handleCloseModal} />}
     </div>
   )
 }
-export const mapDispatchToProps = (dispatch) => {
-  return {
-    addCharacter: function (character) {
-      console.log(character)
-      dispatch(addCharacter(character))
-    },
-    removeCharacter: function (name) {
-      dispatch(removeCharacter(name))
-    }
-  }
-}
-export const mapStateToProps = (state) => ({
-  characters: state.characters
-})
-export default connect(mapStateToProps, mapDispatchToProps)(Characters)
+// export const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addCharacter: function (character) {
+//       console.log(character)
+//       dispatch(addCharacter(character))
+//     },
+//     removeCharacter: function (name) {
+//       dispatch(removeCharacter(name))
+//     }
+//   }
+// }
+// export const mapStateToProps = (state) => ({
+//   characters: state.characters
+// })
+// export default connect(mapStateToProps, mapDispatchToProps)(Characters)
