58550f18daa641fdfbec615975a4717c951789f9
Luciano <lucianoalejandromas@gmail.com>
2023-05-15 12:27:39 -0300

    "cambio de url general"

--- Server/src/index.js
+++ Server/src/index.js
@@ -5,7 +5,10 @@ const { PORT } = process.env
 const router = require("./routes/index")
 const { conn } = require("./DB_connection")
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://rick-and-morty-app-production-98fa.up.railway.app"
+  )
   res.header("Access-Control-Allow-Credentials", "true")
   res.header(
     "Access-Control-Allow-Headers",
